91eb6885e89b4e834d8075be7d18862f0207d28d
Dante <guillemaindante@gmail.com>
2024-11-26 16:59:45 +0800

    fix(eslint): cli-tools/page.tsx is not a module

--- components/ui/coming-soon.tsx
+++ components/ui/coming-soon.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "@/components/ui/navbar";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
 
 const ComingSoon = () => {
   return (
@@ -8,6 +10,8 @@ const ComingSoon = () => {
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-6xl font-extrabold text-center">Coming Soon :)</h1>
       </div>
+      <ShootingStars />
+      <StarsBackground />
     </div>
   );
 };
