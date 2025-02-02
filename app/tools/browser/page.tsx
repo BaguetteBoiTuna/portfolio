21bf162f96ec94fd6ebf25e5da9c560430adbf64
Dante <guillemaindante@gmail.com>
2025-02-02 20:13:22 +0800

    fix(browser):wrong image link

--- app/tools/browser/page.tsx
+++ app/tools/browser/page.tsx
@@ -13,7 +13,7 @@ export default function Browser() {
       content: (
         <div className="h-full w-full flex items-center justify-center text-white">
           <PopupModalImage
-            src="https://arc.net/"
+            src="/arcbrowser.webp"
             width={1000}
             height={800}
             className="h-full w-full object-cover"
