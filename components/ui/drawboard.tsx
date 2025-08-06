dcc0a74ad559aaae9f478a3c8b4b12a9e94eb98e
Dante <guillemaindante@gmail.com>
2025-08-06 20:33:24 +0200

    fix(drawboard): removed extra ws/ in the uri

--- components/ui/drawboard.tsx
+++ components/ui/drawboard.tsx
@@ -31,7 +31,7 @@ const inlineAssetStore: TLAssetStore = {
 
 export default function Drawboard() {
   const store = useSync({
-    uri: `${process.env.NEXT_PUBLIC_SYNC_ENDPOINT}/ws/public-board`,
+    uri: `${process.env.NEXT_PUBLIC_SYNC_ENDPOINT}/public-board`,
     assets: inlineAssetStore,
     shapeUtils: useMemo(() => [...defaultShapeUtils], []),
     bindingUtils: useMemo(() => [...defaultBindingUtils], []),
